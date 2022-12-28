288a299ed697de28f8ab50b2638764e288817e44
Hiroaki Nakamura <hnakamur@gmail.com>
2022-12-28 17:38:47 +0900

    Update safe-integer.lua
    The output:
    ```
    $ luajit safe-integer.lua
    w=9007199254740991, x=9007199254740992, w==x: false
    x=9007199254740992, y=9007199254740992, x==y: true
    w2=-9007199254740991, x2=-9007199254740992, w2==x2: false
    x2=-9007199254740992, y2=-9007199254740992, x2==y2: true
    number 9.0072e+15 is not a safe integer
    number -9.0072e+15 is not a safe integer
    ```

--- safe-integer.lua
+++ safe-integer.lua
@@ -1,22 +1,23 @@
--- See
--- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
--- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MIN_SAFE_INTEGER
 local max_safe_integer = 9007199254740991
 local min_safe_integer = -9007199254740991
+
+local w = max_safe_integer
+local x = max_safe_integer + 1
+local y = max_safe_integer + 2
+print(string.format('w=%d, x=%d, w==x: %s', w, x, w == x))
+print(string.format('x=%d, y=%d, x==y: %s', x, y, x == y))
+
+local w2 = min_safe_integer
+local x2 = min_safe_integer - 1
+local y2 = min_safe_integer - 2
+print(string.format('w2=%d, x2=%d, w2==x2: %s', w2, x2, w2 == x2))
+print(string.format('x2=%d, y2=%d, x2==y2: %s', x2, y2, x2 == y2))
 
 function check_safe_integer(v)
     if v < min_safe_integer or v > max_safe_integer then
         print(string.format("number %g is not a safe integer", v))
     end
 end
-
-local x = max_safe_integer + 1
-local y = max_safe_integer + 2
-print(string.format('x=%d, y=%d, x==y: %s', x, y, x == y))
-
-local x2 = min_safe_integer - 1
-local y2 = min_safe_integer - 2
-print(string.format('x2=%d, y2=%d, x2==y2: %s', x2, y2, x2 == y2))
 
 check_safe_integer(max_safe_integer)
 check_safe_integer(max_safe_integer + 1)
